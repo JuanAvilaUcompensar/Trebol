076912fa159758754c91fe2b0894efe90c3866ec
Juan Windows11 <jmanuelavilaperez@gmail.com>
2023-10-18 11:52:04 -0500

    primer visual

---
+++ app/src/main/java/com/example/trebol/basicMenu.java
@@ -0,0 +1,14 @@
+package com.example.trebol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basicMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_menu);
+    }
+}
---
+++ app/src/main/java/com/example/trebol/login_In.java
@@ -0,0 +1,14 @@
+package com.example.trebol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_in);
+    }
+}
